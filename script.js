30aa0238a064007f704242db17ae39ac589994b4
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-10 16:49:02 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ const text = document.getElementById('carta-gerada');
 
 function texter() {
   const spliter = inputText.value.trim().split(/\s+/); // trim() remove os espaços do início e do final de uma string e .split(/\s+/) separa a string por 1 ou mais espaços;
-  //   console.log(spliter);
   let spliterSpan = '';
   for (let i = 0; i < spliter.length; i += 1) {
     spliterSpan += `<span>${spliter[i]}</span> `;
   }
-  console.log(spliterSpan);
   text.innerHTML = spliterSpan;
+  if (text.innerText === '') {
+    text.innerText = 'Por favor, digite o conteúdo da carta.';
+  }
 }
 
 buttonInput.addEventListener('click', texter);
